7dc56f01ff666b3c5a4221ff23ddaba942c653f3
ReynardPlayz <raphael.reynard.wijaya@gmail.com>
2025-11-27 16:46:38 +0700

    Coba1

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { onMounted } from 'vue'
+import { initFlowbite } from 'flowbite'
+onMounted(() => {
+    initFlowbite();
+})
+
+
+import Hello from './components/icons/Hello.vue';
+</script>
+
+
+<template>
+<Hello/>
+</template>
+
+
+<style scoped>
+
+
+</style>
